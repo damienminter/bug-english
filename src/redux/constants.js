5fe5413d9786b401bf44887ca7c5638dca5a0453
Damien Minter <Damo@MacBook-Pro-2.local>
2020-10-21 17:17:18 +1100

    redux naver api

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -2,3 +2,5 @@ export const ADD_BUG = "ADD_BUG";
 export const SELECT_BUG = "SELECT_BUG";
 export const FILTER_BUGS = "FILTER_BUGS";
 export const SELECT_COMPANY = "SELECT_COMPANY";
+export const SEARCH_NAVER = "SEARCH_NAVER";
+export const SELECT_NAVER = "SELECT_NAVER";
